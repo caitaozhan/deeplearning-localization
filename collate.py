eee13123896b9a53131956187c26fe16d87a2706
caitaozhan <caitaozhan@gmail.com>
2020-12-12 15:07:00 -0500

    fix the deeptxfinder

--- collate.py
+++ collate.py
@@ -24,6 +24,75 @@ default_collate_err_msg_format = (
     "dicts or lists; found {}")
 
 
+# class SensorInputDatasetTranslation(Dataset):
+#     '''Sensor reading input dataset -- for multi TX
+#        Output is image, model as a image segmentation problem
+#     '''
+#     def __init__(self, root_dir: str, transform=None):
+#         '''
+#         Args:
+#             root_dir:  directory with all the images
+#             labels:    labels of images
+#             transform: optional transform to be applied on a sample
+#         '''
+#         self.root_dir = root_dir
+#         self.transform = transform
+#         self.length = len(os.listdir(self.root_dir))
+#         self.sample_per_label = self.get_sample_per_label()
+
+#     def __len__(self):
+#         return self.length * self.sample_per_label
+
+#     def __getitem__(self, idx):
+#         folder = int(idx/self.sample_per_label)
+#         folder = format(folder, '06d')
+#         matrix_name = str(idx%self.sample_per_label) + '.npy'
+#         matrix_path = os.path.join(self.root_dir, folder, matrix_name)
+#         target_name = str(idx%self.sample_per_label) + '.target.npy'
+#         target_img, target_float = self.get_translation_target(folder, target_name)
+#         matrix = np.load(matrix_path)
+#         if self.transform:
+#             matrix = self.transform(matrix)
+#         target_num = np.array([len(target_float)]).astype(np.float32)
+#         sample = {'matrix':matrix, 'target':target_img, 'target_float':target_float, 'target_num':target_num, 'index':idx}
+#         return sample
+
+#     def get_sample_per_label(self):
+#         folder = glob.glob(os.path.join(self.root_dir, '*'))[0]
+#         samples = glob.glob(os.path.join(folder, '*.npy'))
+#         targets = glob.glob(os.path.join(folder, '*.target.npy'))
+#         return len(samples) - len(targets)
+
+#     def get_translation_target(self, folder: str, target_name: str):
+#         '''
+#         Args:
+#             folder      -- eg. 000001
+#             target_name -- eg. 0.target.npy
+#         Return:
+#             np.ndarray, n = 2, the pixels surrounding TX will be assigned some values
+#         '''
+#         location = np.load(os.path.join(self.root_dir, folder, target_name))
+#         num_tx = len(location)
+#         grid = np.zeros((Default.grid_length, Default.grid_length))
+#         for i in range(num_tx):
+#             x, y = location[i][0], location[i][1]
+#             target_float = (x, y)
+#             x, y = int(x), int(y)
+#             neighbor = []
+#             sum_weight = 0
+#             for i in [-1, 0, 1]:
+#                 for j in [-1, 0, 1]:
+#                     nxt = (x + i, y + j)
+#                     if 0 <= nxt[0] < Default.grid_length and 0 <= nxt[1] < Default.grid_length:
+#                         weight = 1./Utility.distance((nxt[0] + 0.5, nxt[1] + 0.5), target_float)
+#                         sum_weight += weight
+#                         neighbor.append((nxt, weight))
+#             for n, w in neighbor:
+#                 grid[n[0]][n[1]] += w / sum_weight * len(neighbor) * 3  # 2 is for adding weights
+#         grid = np.expand_dims(grid, 0)
+#         return grid.astype(np.float32), location.astype(np.float32)
+
+
 class SensorInputDatasetTranslation(Dataset):
     '''Sensor reading input dataset -- for multi TX
        Output is image, model as a image segmentation problem
@@ -49,12 +118,13 @@ class SensorInputDatasetTranslation(Dataset):
         matrix_name = str(idx%self.sample_per_label) + '.npy'
         matrix_path = os.path.join(self.root_dir, folder, matrix_name)
         target_name = str(idx%self.sample_per_label) + '.target.npy'
-        target_img, target_float = self.get_translation_target(folder, target_name)
+        target_arr = self.get_regression_target(folder, target_name)
+        target_num = int(len(target_arr)/2)
         matrix = np.load(matrix_path)
         if self.transform:
             matrix = self.transform(matrix)
-        target_num = np.array([len(target_float)]).astype(np.float32)
-        sample = {'matrix':matrix, 'target':target_img, 'target_float':target_float, 'target_num':target_num, 'index':idx}
+        target_arr = self.min_max_normalize(target_arr)
+        sample = {'matrix': matrix, 'target': target_arr, 'target_num': target_num, 'index': idx}
         return sample
 
     def get_sample_per_label(self):
@@ -63,34 +133,28 @@ class SensorInputDatasetTranslation(Dataset):
         targets = glob.glob(os.path.join(folder, '*.target.npy'))
         return len(samples) - len(targets)
 
-    def get_translation_target(self, folder: str, target_name: str):
+    def get_regression_target(self, folder: str, target_name: str):
         '''
         Args:
-            folder      -- eg. 000001
-            target_name -- eg. 0.target.npy
+            folder: example of folder is 000001
         Return:
-            np.ndarray, n = 2, the pixels surrounding TX will be assigned some values
+            a two dimension matrix
         '''
-        location = np.load(os.path.join(self.root_dir, folder, target_name))
-        num_tx = len(location)
-        grid = np.zeros((Default.grid_length, Default.grid_length))
-        for i in range(num_tx):
-            x, y = location[i][0], location[i][1]
-            target_float = (x, y)
-            x, y = int(x), int(y)
-            neighbor = []
-            sum_weight = 0
-            for i in [-1, 0, 1]:
-                for j in [-1, 0, 1]:
-                    nxt = (x + i, y + j)
-                    if 0 <= nxt[0] < Default.grid_length and 0 <= nxt[1] < Default.grid_length:
-                        weight = 1./Utility.distance((nxt[0] + 0.5, nxt[1] + 0.5), target_float)
-                        sum_weight += weight
-                        neighbor.append((nxt, weight))
-            for n, w in neighbor:
-                grid[n[0]][n[1]] += w / sum_weight * len(neighbor) * 3  # 2 is for adding weights
-        grid = np.expand_dims(grid, 0)
-        return grid.astype(np.float32), location.astype(np.float32)
+        target_file = os.path.join(self.root_dir, folder, target_name)
+        target = np.load(target_file)
+        target = np.reshape(target, -1)
+        return target.astype(np.float32)
+
+    def min_max_normalize(self, target_arr: np.ndarray):
+        '''scale the localization to a range of (0, 1)
+        '''
+        target_arr /= Default.grid_length
+        return target_arr
+
+    def undo_normalize(self, arr: np.ndarray):
+        arr *= self.grid_len
+        return arr
+
 
 class UniformNormalize:
     '''Set a uniform threshold accross all samples
@@ -107,12 +171,74 @@ tf = T.Compose([
      UniformNormalize(Default.noise_floor),                 # TUNE: Uniform normalization is better than the above minmax normalization
      T.ToTensor()])
 
+# def my_padding(batch, max_len):
+#     """add zeros to elements that are not maximum length"""
+#     for i in range(len(batch)):
+#         diff = max_len - len(batch[i])
+#         if diff > 0:                      # padding
+#             zeros = torch.zeros(diff, 2)
+#             padded = torch.cat((batch[i], zeros), 0)
+#             batch[i] = padded
+
+# def my_collate(batch):
+#     """Puts each data field into a tensor with outer dimension batch size"""
+#     elem = batch[0]
+#     elem_type = type(elem)
+#     if isinstance(elem, torch.Tensor):
+#         max_len = len(max(batch, key=len))
+#         min_len = len(min(batch, key=len))
+#         if max_len != min_len:
+#             my_padding(batch, max_len)
+#         out = None
+#         if torch.utils.data.get_worker_info() is not None:
+#             # If we're in a background process, concatenate directly into a
+#             # shared memory tensor to avoid an extra copy
+#             numel = sum([x.numel() for x in batch])
+#             storage = elem.storage()._new_shared(numel)
+#             out = elem.new(storage)
+#         # max_len = len(max(batch, key=len))
+#         # min_len = len(min(batch, key=len))
+#         # if max_len != min_len:
+#         #     my_padding(batch, max_len)
+#         return torch.stack(batch, 0, out=out)
+#     elif elem_type.__module__ == 'numpy' and elem_type.__name__ != 'str_' \
+#             and elem_type.__name__ != 'string_':
+#         if elem_type.__name__ == 'ndarray' or elem_type.__name__ == 'memmap':
+#             # array of string classes and object
+#             if np_str_obj_array_pattern.search(elem.dtype.str) is not None:
+#                 raise TypeError(default_collate_err_msg_format.format(elem.dtype))
+
+#             return my_collate([torch.as_tensor(b) for b in batch])
+#         elif elem.shape == ():  # scalars
+#             return torch.as_tensor(batch)
+#     elif isinstance(elem, float):
+#         return torch.tensor(batch, dtype=torch.float64)
+#     elif isinstance(elem, int_classes):
+#         return torch.tensor(batch)
+#     elif isinstance(elem, string_classes):
+#         return batch
+#     elif isinstance(elem, container_abcs.Mapping):
+#         return {key: my_collate([d[key] for d in batch]) for key in elem}
+#     elif isinstance(elem, tuple) and hasattr(elem, '_fields'):  # namedtuple
+#         return elem_type(*(my_collate(samples) for samples in zip(*batch)))
+#     elif isinstance(elem, container_abcs.Sequence):
+#         # check to make sure that the elements in batch have consistent size
+#         it = iter(batch)
+#         elem_size = len(next(it))
+#         if not all(len(elem) == elem_size for elem in it):
+#             raise RuntimeError('each element in list of batch should be of equal size')
+#         transposed = zip(*batch)
+#         return [my_collate(samples) for samples in transposed]
+
+#     raise TypeError(default_collate_err_msg_format.format(elem_type))
+
+
 def my_padding(batch, max_len):
     """add zeros to elements that are not maximum length"""
     for i in range(len(batch)):
         diff = max_len - len(batch[i])
         if diff > 0:                      # padding
-            zeros = torch.zeros(diff, 2)
+            zeros = torch.zeros(diff)
             padded = torch.cat((batch[i], zeros), 0)
             batch[i] = padded
 
@@ -169,7 +295,7 @@ def my_collate(batch):
     raise TypeError(default_collate_err_msg_format.format(elem_type))
 
 
-root_dir = './data/matrix-train51'
+root_dir = './data/205train'
 sensor_input_dataset = SensorInputDatasetTranslation(root_dir = root_dir, transform = tf)
 sensor_input_dataloader = DataLoader(sensor_input_dataset, batch_size=32, shuffle=True, num_workers=3, collate_fn=my_collate)
 
@@ -180,7 +306,7 @@ def my_uncollate(y_num, y_float):
     """this is for uncollating the target_float"""
     y_float_tmp = []
     for ntx, y_f in zip(y_num, y_float):
-        y_float_tmp.append(y_f[:int(ntx[0]+1e-6)])
+        y_float_tmp.append(y_f[:ntx*2])
     return np.array(y_float_tmp)
 
 
@@ -189,8 +315,8 @@ for t, sample in enumerate(sensor_input_dataloader):
     # y = sample['target'].to(device)
     # y_num   = sample['target_num'].to(device)
     y_num2  = np.array(sample['target_num'])
-    y_float = np.array(sample['target_float'])
-    y_float = my_uncollate(y_num2, y_float)
+    y = np.array(sample['target'])
+    y = my_uncollate(y_num2, y)
     if t % 10 == 9:
         print(t, end=' ')
 print('caitao')
